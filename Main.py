a8bb32797d310cfc365f01a60e667a4eab84ad68
Leander Tenbarge <leo.tenbarge@gmail.com>
2025-10-24 16:01:20 -0500

    Updated to Use GMSH
    Reorganized imports and updated geometry creation using GMSH API.

--- Main.py
+++ Main.py
@@ -1,24 +1,46 @@
 # Leander Tenbarge
-import CreateLoft
+
+
+#Import CreateLoft
 import WingLogic
 import InputOutput
-
-
-FilePath = "/Users/leandertenbarge/Desktop/Multi-Element scripting/CaseFolder"
+import CreateLoft
+import gmsh
+FilePath = "/Users/leandertenbarge/Desktop/3dCAD/Multi-Element-Wing-Generator/CaseFolder"
 FileName = 'MultiElementWing'
 NumSections = 50 
 Scale = 1.5
 
 
+gmsh.initialize()
+gmsh.model.add("WingModel")
+
+# Parsing the Input Files and defining the points:
 WingSections = WingLogic.run(FilePath,NumSections)
 InterpolatedArray = InputOutput.parse(FilePath)
 EndplateData = InputOutput.parseEndPlate(FilePath)
-Endplates = CreateLoft.BuildEndplate(InterpolatedArray,EndplateData)
-WireFrame = CreateLoft.CreateWireFrame(WingSections)
-Wings = CreateLoft.BuildLoft(WireFrame)
-Geometry = CreateLoft.CreateGeometry(Wings,Endplates)
-ScaledGeometry = CreateLoft.Scale(Scale,Geometry)
-InputOutput.Export(ScaledGeometry,FileName,FilePath)
+
+# Creating the geometry Utilizing the GMSH API
+Endplates = CreateLoft.buildEndplate(InterpolatedArray,EndplateData)
+WireFrame = CreateLoft.createWireFrame(WingSections)
+Wings = CreateLoft.buildLoft(WireFrame)
+ScaledGeometry = CreateLoft.fuseScale(Scale=Scale,Wings=Wings,Endplates=Endplates)
+Domain = CreateLoft.buildDomain(Height=5,Width=5,Ground=.5,Forward=5,Backward=10,Scale=Scale)
+CreateLoft.domainBoolean(Wing=ScaledGeometry,Box=Domain)
+
+
+# Exporting
+gmsh.write("my_model.iges")
+
+
+gmsh.fltk.run()
+gmsh.finalize()
+
+
+
+
+
+
 
 
 
